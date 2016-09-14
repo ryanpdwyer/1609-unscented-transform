d1da3a3ce30631c11018c285f0291dd776f1a27b
Ryan Dwyer <ryanpdwyer@gmail.com>
2016-09-14 17:12:23 -0400

    Presentation version

--- notebooks/unscentedtransform.py
+++ notebooks/unscentedtransform.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+What is the unscented transform?
+================================
+
+Helper functions for a talk on the unscented transform.
+
+"""
 from __future__ import division, print_function
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -10,7 +18,8 @@ from uncertainties import umath, ufloat
 from matplotlib.patches import Ellipse
 from matplotlib import colors
 
-def svdsqrtm(x, eps=1e-13):
+
+def svdsqrtm(x, eps=1e-15):
     """Return the matrix square root of x calculating using the svd.
     
     Set singular values < eps to 0.
@@ -25,25 +34,36 @@ def svdsqrtm(x, eps=1e-13):
     return np.dot(u, np.dot(np.diag(np.sqrt(s_pos)), v.T))
 
 
-def unscented(x, P):
+def unscented(x, P, scale=1.0):
     """For a vector x of length N with covariance matrix P,
-    form 2N sigma points used for taking the unscented transform."""
-    N = x.size
-    Psqrt = svdsqrtm(N * P)
-    x_sigma = np.zeros((N, 2*N))
-    for i in xrange(N):
-        x_sigma[:, i] = x + Psqrt[:, i]
+    form 2N sigma points used for taking the unscented transform.
 
-    for i in xrange(N, 2*N):
-        x_sigma[:, i] = x - Psqrt[:, i-N]
+                                      __
+    Defaults to taking points at x ± √NP."""
+    x = x.ravel() # Force shape
+    N = x.size
+    Psqrt = scale * svdsqrtm(N * P)
+    x_sigma = []
+
+    for i in xrange(N):
+        x_sigma.append(x + Psqrt[:, i])
+
+    for i in xrange(N):
+        x_sigma.append(x - Psqrt[:, i])
 
     return x_sigma
 
 def unscented_mu_cov(x_sigma):
-    N, N2 = x_sigma.shape
-    x_mu = np.mean(x_sigma, axis=1)
-    diff = x_sigma - x_mu.reshape((-1, 1))
-    x_cov = np.dot(diff, diff.T) / N2
+    """Approximate mean, covariance from 2N sigma points transformed through
+    an arbitrary non-linear transformation.
+
+    Returns a flattened 1d array for x."""
+    N = len(x_sigma)
+    pts = np.array(x_sigma)
+
+    x_mu = np.mean(pts, axis=0)
+    diff = pts - x_mu
+    x_cov = np.dot(diff.T, diff) / N
     return x_mu, x_cov
 
 phis = np.linspace(0, 2*np.pi, 101)
@@ -89,12 +109,13 @@ def magtime_phi_linear(mu_phi, sigma_A, sigma_phi):
     
     # Mean circle (mu_A = 1)
     ax.plot(np.cos(phis), np.sin(phis))
+    Amax = 1+sigma_A
     fill_donut(ax, [1-sigma_A, 1+sigma_A], edgecolor='none', alpha=0.1)
     
     
     ax.plot([0, np.cos(mu_phi)], [0, np.sin(mu_phi)], 'g')
     phi_plot = np.linspace(mu_phi+sigma_phi, mu_phi-sigma_phi, 9)
-    ax.fill(np.r_[0, np.cos(phi_plot)], np.r_[0, np.sin(phi_plot)], 'g', alpha=0.1)
+    ax.fill(np.r_[0, Amax*np.cos(phi_plot)], Amax*np.r_[0, np.sin(phi_plot)], 'g', alpha=0.1)
     ax.plot(np.cos(mu_phi), np.sin(mu_phi), 'ko', markeredgecolor='none')
     
     # Propogate error
@@ -122,6 +143,8 @@ def magtime_phi_exact(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     fig, ax = plt.subplots(figsize=(8,8))
     axis_color='0.5'
     
+    Amax = 1+sigma_A
+
     # Add exact propogation of samples
     pts = 500
     A_pts = np.random.randn(pts)*sigma_A + 1
@@ -143,7 +166,7 @@ def magtime_phi_exact(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     
     ax.plot([0, np.cos(mu_phi)], [0, np.sin(mu_phi)], 'g')
     phi_plot = np.linspace(mu_phi+sigma_phi, mu_phi-sigma_phi, 9)
-    ax.fill(np.r_[0, np.cos(phi_plot)], np.r_[0, np.sin(phi_plot)], 'g', alpha=0.1)
+    ax.fill(np.r_[0, Amax*np.cos(phi_plot)], np.r_[0, Amax*np.sin(phi_plot)], 'g', alpha=0.1)
     ax.plot(np.cos(mu_phi), np.sin(mu_phi), color='c', marker='o', markeredgecolor='none')
     # KDE plot is the right idea, but too cluttered right now.
     #     sns.kdeplot(x_pts, y_pts, ax=ax, cmap=plt.cm.Blues)
@@ -169,6 +192,8 @@ def magtime_phi_unscented(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     phi_x = phis[:51]
     fig, ax = plt.subplots(figsize=(8,8))
     axis_color='0.5'
+
+    Amax = 1 + sigma_A
     
     # Add exact propogation of samples
     pts = 500
@@ -184,15 +209,11 @@ def magtime_phi_unscented(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     x = np.array([1.0, mu_phi])
     cov = np.diag([sigma_A**2, sigma_phi**2])
     X = unscented(x, cov)
-    Y = np.zeros_like(X)
-    for i, col in enumerate(X.T):
-        Y[0, i] = col[0] * np.cos(col[1])
-        Y[1, i] = col[0] * np.sin(col[1])
+    Y = [np.array([x[0]*np.cos(x[1]),
+                   x[0]*np.sin(x[1])]) for x in X]
 
-    np.mean(Y, axis=1)
     mu_y, cov_y = unscented_mu_cov(Y)
-    
-    
+
     # Add axes
     ax.axhline(color=axis_color)
     ax.axvline(color=axis_color)
@@ -204,7 +225,7 @@ def magtime_phi_unscented(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     
     ax.plot([0, np.cos(mu_phi)], [0, np.sin(mu_phi)], 'g')
     phi_plot = np.linspace(mu_phi+sigma_phi, mu_phi-sigma_phi, 9)
-    ax.fill(np.r_[0, np.cos(phi_plot)], np.r_[0, np.sin(phi_plot)], 'g', alpha=0.1)
+    ax.fill(np.r_[0, Amax*np.cos(phi_plot)], np.r_[0, Amax*np.sin(phi_plot)], 'g', alpha=0.1)
     ax.plot(np.cos(mu_phi), np.sin(mu_phi), color='c', marker='o', markeredgecolor='none')
     
     # Propogate error
@@ -220,7 +241,8 @@ def magtime_phi_unscented(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     ax.scatter(*mu_xy, s=50, c=pt_color, edgecolors='none')
     ax.add_artist(cov_ellipse(mu_xy, cov_pts, edgecolor=pt_color, fill=False, lw=2.0))
     
-    ax.scatter(Y[0], Y[1], c='m', s=35, edgecolors='none')
+    Yarray = np.array(Y).T
+    ax.scatter(Yarray[0], Yarray[1], c='m', s=35, edgecolors='none')
     ax.scatter(*mu_y, c='m', s=50, edgecolors='none')
     ax.add_artist(cov_ellipse(mu_y, cov_y, edgecolor='m', fill=False, lw=2.0))
 
@@ -228,3 +250,163 @@ def magtime_phi_unscented(mu_phi, sigma_A, sigma_phi, xlim, ylim):
     
     ax.set_xlim(*xlim)
     ax.set_ylim(*ylim)
+
+
+x0 = np.array([1.0, 0.0, 1.0]).reshape((1, -1))
+
+def step_time(F, x, P, Q):
+    xm = np.dot(F, x)
+    Pm = np.dot(np.dot(F, P), F.T) + Q
+    return xm, Pm
+
+def Hx(x):
+    u"""For a state vector x (A, φ, ω), return derivatives
+    dy/dx at the current state."""
+    xx = x.ravel()
+    return np.array([np.cos(xx[1]),
+                    -xx[0] * np.sin(xx[1]),
+                    0.0]).reshape((1, -1))
+
+def Yfunc(x):
+    """Evaluate position y for a state vector x (A, φ, ω)."""
+    return x[0]*np.cos(x[1])
+
+
+def measure_EKF(Yfunc, Hfunc, xm, Pm, R, y_meas):
+    H = Hfunc(xm)
+
+    y_est = float(Yfunc(xm))
+    K = np.dot(Pm, H.T) / float(np.dot(np.dot(H, Pm), H.T) + R)
+    err = float(y_meas - y_est)
+    xp = xm + K * err
+
+    one_minus_KH = (np.eye(xm.size) - np.dot(K, H))
+    Pp = np.dot(np.dot(one_minus_KH, Pm), one_minus_KH.T) + R * np.dot(K, K.T)
+    return y_est, xp, Pp
+
+
+def measure_UKF(Yfunc, xm, Pm, R, y_meas):
+    X = unscented(xm, Pm)
+    Y = [Yfunc(x) for x in X]
+
+    y_est, Py_no_r = unscented_mu_cov(Y)
+
+    Py = float(Py_no_r + R)
+    x_diff = np.array(X) - xm.ravel()
+    y_diff = (np.array(Y) - y_est).reshape((-1, 1))
+
+    Pxy = np.dot(x_diff.T, y_diff)
+
+
+    err = float(y_meas - y_est) # 1x1
+
+    # Now do the measurement update
+    # Only works for single measurement!
+    K = Pxy / Py
+    xp = xm +  K * err
+    Pp = Pm - np.dot(K, Py * K.T)
+    return y_est, xp, Pp
+
+
+class EKF(object):
+    def __init__(self, F, Q, R, Yfunc, Hfunc, x, P):
+        self.F = F
+        self.Q = Q
+        self.R = R
+        self.Hf = Hfunc
+        self.Yf = Yfunc
+        self.M = F.shape[0]  # Dimension of state vector
+        self.x = x
+        self.P = P    
+    
+    def measure(self, y_meas):
+        self.xm = self.x
+        self.Pm = self.P
+
+        y_est, xp, Pp = measure_EKF(self.Yf, self.Hf, self.x, self.P, self.R, y_meas)
+    
+        self.x = xp
+        self.P = Pp
+
+        return y_est, xp, Pp
+        
+    def step(self):
+        self.xp = self.x
+        self.Pp = self.P
+        x, P = step_time(self.F, self.x, self.P, self.Q)
+        self.x = x
+        self.P = P
+        
+        return x, P
+
+class UKF(object):
+    def __init__(self,  F, Q, R, Yfunc, x, P):
+        self.F = F
+        self.Q = Q
+        self.R = R
+        self.Yf = Yfunc
+        self.M = F.shape[0]  # Dimension of state vector
+        self.x = x
+        self.P = P
+    
+    def step(self):
+        self.xp = self.x
+        self.Pp = self.P
+        x, P = step_time(self.F, self.x, self.P, self.Q)
+        self.x = x
+        self.P = P
+        
+        return x, P
+    
+    def measure(self, y_meas):
+        self.xm = self.x
+        self.Pm = self.P
+
+        y_est, xp, Pp = measure_UKF(self.Yf, self.x, self.P, self.R, y_meas)
+    
+        self.x = xp
+        self.P = Pp
+        
+        return y_est, xp, Pp
+    
+class Sim(object):
+    def __init__(self, F, Q, R, Yfunc, x0, yield_x=True):
+        self.F = F
+        self.Q = Q
+        self.R = R
+        self.Yf = Yfunc
+        self.Mx = x0.size  # Dimension of state vector
+        self.x = x0
+        self.yield_x = yield_x
+        
+        y = Yfunc(x0)
+        self.My = 1
+    
+    # Python 3 compatibility
+    def __next__(self):
+        return self.next()
+    
+    def next(self):
+        x_next = np.dot(self.F, self.x) + np.dot(self.Q, np.random.randn(self.Mx, 1))
+        self.x = x_next
+        y_next = float(Yfunc(x_next) + np.random.randn(self.My) * self.R)
+        
+        if self.yield_x:
+            return x_next, y_next
+        else:
+            return y_next
+        
+def Hx(x):
+    u"""For a state vector x (A, φ, ω), return derivatives
+    dy/dx at the current state."""
+    x = x.ravel()
+    return np.array([
+            np.cos(x[1]),
+            -x[0] * np.sin(x[1]), 0.0]).reshape((1, -1))
+
+def Yfunc(x):
+    """Evaluate position y for a state vector x (A, φ, ω)."""
+    x = x.ravel()
+    return float(x[0]*np.cos(x[1]))
+
+
